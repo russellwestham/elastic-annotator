7024c4ce0e8c53f3cf868d9cdaabe4ac928cee7d
Lee Kunhee <kunhee8@naver.com>
2026-03-26 19:22:32 +0900

    Route match-based annotate URL directly to AnnotationPage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 
 import { AnnotationPage } from "./pages/AnnotationPage";
-import { MatchAnnotateRedirectPage } from "./pages/MatchAnnotateRedirectPage";
 import { MatchRedirectPage } from "./pages/MatchRedirectPage";
 import { SessionCreatePage } from "./pages/SessionCreatePage";
 
@@ -10,7 +9,7 @@ function App() {
     <Routes>
       <Route path="/" element={<SessionCreatePage />} />
       <Route path="/m/:matchId" element={<MatchRedirectPage />} />
-      <Route path="/annotate/m/:matchId" element={<MatchAnnotateRedirectPage />} />
+      <Route path="/annotate/m/:matchId" element={<AnnotationPage />} />
       <Route path="/annotate/:sessionId" element={<AnnotationPage />} />
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
